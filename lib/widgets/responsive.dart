be54bcaa3cdc3cd80de530280e73b673e55516a9
Choyon Bonik <choyonbonik@gmail.com>
2023-02-27 15:07:50 +0600

    responsive widget

--- lib/widgets/responsive.dart
+++ lib/widgets/responsive.dart
@@ -11,7 +11,7 @@ class ResponsiveWidget extends StatelessWidget {
     this.smallScreen,
   }) : super(key: key);
 
-  static bool isSmallScreen(BuildContext context){
+  static bool isSmallScreen(BuildContext context) {
     return MediaQuery.of(context).size.width < 800;
   }
 
@@ -19,20 +19,21 @@ class ResponsiveWidget extends StatelessWidget {
     return MediaQuery.of(context).size.width > 1200;
   }
 
-  static bool isMediumScreen(BuildContext context){
-  return MediaQuery.of(context).size.width >= 800 && MediaQuery.of(context).size.width <= 1200;
+  static bool isMediumScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width >= 800 &&
+        MediaQuery.of(context).size.width <= 1200;
   }
+
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constrints){
-      if(constrints.maxWidth > 1200){
+    return LayoutBuilder(builder: (context, constrints) {
+      if (constrints.maxWidth > 1200) {
         return largeScreen;
-      } else if(constrints.maxWidth <= 1200 && constrints.maxWidth >= 800) {
+      } else if (constrints.maxWidth <= 1200 && constrints.maxWidth >= 800) {
         return mediumScreen ?? largeScreen;
       } else {
         return smallScreen ?? largeScreen;
       }
-
     });
   }
 }
