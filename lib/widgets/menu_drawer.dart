3578235e20821b4318d8f03ac0d900cabc9c2b78
Choyon Bonik <choyonbonik@gmail.com>
2023-03-02 14:05:44 +0600

    menu drawer widget

--- lib/widgets/menu_drawer.dart
+++ lib/widgets/menu_drawer.dart
@@ -6,90 +6,108 @@ class MenuDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Container(
-        color: Color(0xFF6158e6),
-        child: Padding(
-            padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              InkWell(
-                onTap: (){},
-                child: Text(
-                  'Home',
-                  style: TextStyle(
-                    color: Colors.white, fontSize: 26,
-                  ),
-                ),
+      child: Column(
+        children: [
+          DrawerHeader(
+            child: Container(
+              height: 500,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('images/background.png'),
+                    fit: BoxFit.cover),
               ),
-              Padding(
-                  padding: EdgeInsets.only(
-                top: 5.0, bottom: 5.0,
-              ),
-                child: Divider(
-                  color: Colors.blueGrey.shade400,
-                  thickness: 2,
-                ),
-              ),
-              InkWell(
-                onTap: (){},
-                child: Text(
-                  'About',
-                  style: TextStyle(
-                    color: Colors.white, fontSize: 26,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                  top: 5.0, bottom: 5.0,
-                ),
-                child: Divider(
-                  color: Colors.blueGrey.shade400,
-                  thickness: 2,
-                ),
-              ),
-              InkWell(
-                onTap: (){},
-                child: Text(
-                  'History',
-                  style: TextStyle(
-                    color: Colors.white, fontSize: 26,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                  top: 5.0, bottom: 5.0,
-                ),
-                child: Divider(
-                  color: Colors.blueGrey.shade400,
-                  thickness: 2,
-                ),
-              ),
-              InkWell(
-                onTap: (){},
-                child: Text(
-                  'Contact',
-                  style: TextStyle(
-                    color: Colors.white, fontSize: 26,
-                  ),
-                ),
-              ),
-              Expanded(
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Text(
-                      'Copyright @ 2023 | Choyon',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                      ),
-                    ),
-                  )
-              )
-            ],
+            ),
           ),
-        ),
+          InkWell(
+            onTap: () {},
+            child: Text(
+              'Home',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: 5.0,
+              bottom: 5.0,
+            ),
+            child: Divider(
+              color: Colors.blueGrey.shade400,
+              thickness: 2,
+              indent: 20,
+              endIndent: 20,
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: Text(
+              'About',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: 5.0,
+              bottom: 5.0,
+            ),
+            child: Divider(
+              color: Colors.blueGrey.shade400,
+              thickness: 2,
+              indent: 20,
+              endIndent: 20,
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: Text(
+              'History',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: 5.0,
+              bottom: 5.0,
+            ),
+            child: Divider(
+              color: Colors.blueGrey.shade400,
+              thickness: 2,
+              indent: 20,
+              endIndent: 20,
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: Text(
+              'Contact',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: 370,
+            ),
+            child: Text(
+              'Copyright @ 2023 | Choyon',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 14,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
