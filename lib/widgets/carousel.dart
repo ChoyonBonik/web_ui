752c80ec225dcfccefd178e667ff8ebbb95f95c6
Choyon Bonik <choyonbonik@gmail.com>
2023-03-02 14:07:04 +0600

    carousel widget

--- lib/widgets/carousel.dart
+++ lib/widgets/carousel.dart
@@ -37,14 +37,17 @@ class _MainCarouselState extends State<MainCarousel> {
   ];
 
   List<Widget> generateImageTiles(screenSize) {
-    return images.map((element) => ClipRRect(
-      borderRadius: BorderRadius.circular(8.0),
-      child: Image.asset(
-        element,
-        fit: BoxFit.cover,
-      ),
-    ),
-    ).toList();
+    return images
+        .map(
+          (element) => ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.asset(
+              element,
+              fit: BoxFit.cover,
+            ),
+          ),
+        )
+        .toList();
   }
 
   @override
@@ -56,22 +59,22 @@ class _MainCarouselState extends State<MainCarousel> {
         CarouselSlider(
           items: imageSliders,
           options: CarouselOptions(
-          enlargeCenterPage: true,
-          aspectRatio: 18 / 8,
-          autoPlay: true,
-          onPageChanged: (index, reason){
-            setState(() {
-              _current = index;
-              for (int i = 0; i < imageSliders.length; i++) {
-                if (i == index) {
-                  _isSelected[i] = true;
-                } else {
-                  _isSelected[i] = false;
+            enlargeCenterPage: true,
+            aspectRatio: 18 / 8,
+            autoPlay: true,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _current = index;
+                for (int i = 0; i < imageSliders.length; i++) {
+                  if (i == index) {
+                    _isSelected[i] = true;
+                  } else {
+                    _isSelected[i] = false;
+                  }
                 }
-              }
-            });
-          },
-        ),
+              });
+            },
+          ),
           carouselController: _controller,
         ),
         AspectRatio(
@@ -81,89 +84,96 @@ class _MainCarouselState extends State<MainCarousel> {
               places[_current],
               style: TextStyle(
                 letterSpacing: 8,
-                 fontFamily: 'Electrolize',
+                fontFamily: 'Electrolize',
                 fontSize: screenSize.width / 25,
                 color: Colors.white,
               ),
             ),
           ),
         ),
-        screenSize.width < 800? Container():
-        AspectRatio(
-            aspectRatio: 17 / 8,
-          child: Center(
-            heightFactor: 1,
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(padding: EdgeInsets.only(
-                left: screenSize.width / 8,
-                right: screenSize.width / 8,
-              ),
-                child: Card(
-                  elevation: 5,
-                  child: Padding(padding: EdgeInsets.only(
-                    top: screenSize.height / 50,
-                    bottom: screenSize.height / 50,
-                  ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        for (int i = 0; i < places.length; i++)
-                          Column(
-                            mainAxisSize: MainAxisSize.min,
+        screenSize.width < 800
+            ? Container()
+            : AspectRatio(
+                aspectRatio: 17 / 8,
+                child: Center(
+                  heightFactor: 1,
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        left: screenSize.width / 8,
+                        right: screenSize.width / 8,
+                      ),
+                      child: Card(
+                        elevation: 5,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            top: screenSize.height / 50,
+                            bottom: screenSize.height / 50,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
-                              InkWell(
-                                splashColor: Colors.transparent,
-                                hoverColor: Colors.transparent,
-                                onHover: (value){
-                                  setState(() {
-                                    value? _isHovering[i] = true : _isHovering[i] = false;
-                                  });
-                                },
-                                onTap: (){
-                                  _controller.animateToPage(i);
-                                },
-                                child: Padding(padding: EdgeInsets.only(
-                                  top: screenSize.height / 80,
-                                  bottom: screenSize.height / 90,
-                                ),
-                                  child: Text(
-                                    places[i],
-                                    style: TextStyle(
-                                      color: _isHovering[i] ? Colors.blueGrey[900] : Colors.blueGrey,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Visibility(
-                                maintainSize: true,
-                                  maintainAnimation: true,
-                                  maintainState: true,
-                                  visible: _isSelected[i],
-                                  child: AnimatedOpacity(
-                                    opacity: _isSelected[i] ? 1 : 0,
-                                    duration: Duration(milliseconds: 400),
-                                    child: Container(
-                                      height: 5,
-                                        decoration: BoxDecoration(
-                                          color: Colors.blueGrey,
-                                          borderRadius: BorderRadius.all(
-                                            Radius.circular(10),
-                                          )
+                              for (int i = 0; i < places.length; i++)
+                                Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    InkWell(
+                                      splashColor: Colors.transparent,
+                                      hoverColor: Colors.transparent,
+                                      onHover: (value) {
+                                        setState(() {
+                                          value
+                                              ? _isHovering[i] = true
+                                              : _isHovering[i] = false;
+                                        });
+                                      },
+                                      onTap: () {
+                                        _controller.animateToPage(i);
+                                      },
+                                      child: Padding(
+                                        padding: EdgeInsets.only(
+                                          top: screenSize.height / 80,
+                                          bottom: screenSize.height / 90,
                                         ),
-                                      width: screenSize.width / 10,
+                                        child: Text(
+                                          places[i],
+                                          style: TextStyle(
+                                            color: _isHovering[i]
+                                                ? Colors.blueGrey[900]
+                                                : Colors.blueGrey,
+                                          ),
+                                        ),
+                                      ),
                                     ),
-                                  ))
+                                    Visibility(
+                                        maintainSize: true,
+                                        maintainAnimation: true,
+                                        maintainState: true,
+                                        visible: _isSelected[i],
+                                        child: AnimatedOpacity(
+                                          opacity: _isSelected[i] ? 1 : 0,
+                                          duration: Duration(milliseconds: 400),
+                                          child: Container(
+                                            height: 5,
+                                            decoration: BoxDecoration(
+                                                color: Colors.blueGrey,
+                                                borderRadius: BorderRadius.all(
+                                                  Radius.circular(10),
+                                                )),
+                                            width: screenSize.width / 10,
+                                          ),
+                                        ))
+                                  ],
+                                )
                             ],
-                          )
-                      ],
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-          ),
-        )
+              )
       ],
     );
   }
