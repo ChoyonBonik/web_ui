2ce7cd28563ae8217d9b7781d878961a6e8fe101
Choyon Bonik <choyonbonik@gmail.com>
2023-03-02 14:06:17 +0600

    bottom bar widget

--- lib/widgets/bottom_bar.dart
+++ lib/widgets/bottom_bar.dart
@@ -15,65 +15,131 @@ class BottomBar extends StatelessWidget {
         borderRadius: BorderRadius.all(
           Radius.circular(0.0),
         ),
-        gradient: LinearGradient(colors: [
-          gradientStartColor,
-          gradientEndColor
-        ],
-            begin: const FractionalOffset(0.2, 0.2),
-            end: const FractionalOffset(1.0, 1.0),
-            stops: [0.0, 1.0],
-            tileMode: TileMode.clamp,
+        gradient: LinearGradient(
+          colors: [gradientStartColor, gradientEndColor],
+          begin: const FractionalOffset(0.2, 0.2),
+          end: const FractionalOffset(1.0, 1.0),
+          stops: [0.0, 1.0],
+          tileMode: TileMode.clamp,
         ),
       ),
       padding: EdgeInsets.all(30),
-      child: Column(
-        children: [
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              BottomBarColumn(
-                  heading: 'ABOUT', s1: 'Contact Us', s2: 'About Us', s3: 'Careers'),
-              BottomBarColumn(
-                  heading: 'HELP', s1: 'Payment', s2: 'Cancellation', s3: 'FAQ'),
-              BottomBarColumn(
-                  heading: 'SOCIAL', s1: 'Facebook', s2: 'LinkIn', s3: 'Twitter'),
-              Container(
-                color: Colors.white,
-                width: 2,
-                height: 150,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  InfoText(
-                    type: 'Email',
-                    text: 'choyonbonik@gmail.com',
-                  ),
-                  SizedBox(height: 5),
-                  InfoText(
-                    type: 'Address',
-                    text: '88/3, Sukrabad, Dhanmondi-32, Dhaka-1205',
-                  ),
+      child: MediaQuery.of(context).size.width < 800
+          ? Column(
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    BottomBarColumn(
+                        heading: 'ABOUT',
+                        s1: 'Contact Us',
+                        s2: 'About Us',
+                        s3: 'Careers'),
+                    BottomBarColumn(
+                        heading: 'HELP',
+                        s1: 'Payment',
+                        s2: 'Cancellation',
+                        s3: 'FAQ'),
+                    BottomBarColumn(
+                        heading: 'SOCIAL',
+                        s1: 'Facebook',
+                        s2: 'LinkIn',
+                        s3: 'Twitter'),
                   ],
-              ),
-            ],
-          ),
-          Divider(
-            color: Colors.white,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            'Copyright © 2023 | Choyon',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 14,
+                ),
+                Divider(
+                  color: Colors.white60,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    InfoText(
+                      type: 'Email',
+                      text: 'choyonbonik@gmail.com',
+                    ),
+                    SizedBox(height: 5),
+                    InfoText(
+                      type: 'Address',
+                      text: '88/3, Sukrabad, Dhanmondi-32, Dhaka-1205',
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Divider(
+                  color: Colors.white60,
+                ),
+                Text(
+                  'Copyright © 2023 | Choyon',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            )
+          : Column(
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    BottomBarColumn(
+                        heading: 'ABOUT',
+                        s1: 'Contact Us',
+                        s2: 'About Us',
+                        s3: 'Careers'),
+                    BottomBarColumn(
+                        heading: 'HELP',
+                        s1: 'Payment',
+                        s2: 'Cancellation',
+                        s3: 'FAQ'),
+                    BottomBarColumn(
+                        heading: 'SOCIAL',
+                        s1: 'Facebook',
+                        s2: 'LinkIn',
+                        s3: 'Twitter'),
+                    Container(
+                      color: Colors.white,
+                      width: 2,
+                      height: 150,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        InfoText(
+                          type: 'Email',
+                          text: 'choyonbonik@gmail.com',
+                        ),
+                        SizedBox(height: 5),
+                        InfoText(
+                          type: 'Address',
+                          text: '88/3, Sukrabad, Dhanmondi-32, Dhaka-1205',
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Divider(
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Copyright © 2023 | Choyon',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                )
+              ],
             ),
-          )
-        ],
-      ),
     );
   }
 }
